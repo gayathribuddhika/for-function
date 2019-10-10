c08062487edb483b1b3b2d326fb9c44f4effa76b
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-10-10 19:14:59 +0530

    Add files via upload

---
+++ test02.py
@@ -0,0 +1,23 @@
+
+subjects = ["Maths, Sinhala, English, Science, "]
+
+for s in subjects:
+    print(s)
+    print(len(s))
+
+print("Marks of Maths of 5 students")
+
+marks = [25, 78, 36, 96, 78]
+
+for m in marks[:2]:
+    print(m)
+
+food = ['rice', 'fish', 'chicken', 'beans']
+
+for f in food:
+    print(f)
+    print(len(f))
+
+for f in food[:2]:
+    print(f)
+    print(len(f))
